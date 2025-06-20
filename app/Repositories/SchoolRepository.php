202e9e4a8a9a69a85bfe466235b2bc49fd8e8253
AndresGutierrezHurtado <andres52885241@gmail.com>
2025-06-19 22:48:00 -0500

    feat: implement SchoolRepository for managing school data with CRUD operations

---
+++ app/Repositories/SchoolRepository.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Contracts\SchoolRepositoryInterface;
+use App\Models\School;
+
+class SchoolRepository implements SchoolRepositoryInterface
+{
+    public function getAll(): array
+    {
+        return School::all()->toArray();
+    }
+
+    public function getById(string $id): array
+    {
+        return School::with('products')->find($id)->toArray();
+    }
+
+    public function create(array $data): array
+    {
+        $createdSchool = School::create($data);
+        $id = $createdSchool->school_id;
+
+        return $this->getById($id);
+    }
+
+    public function update(string $id, array $data): bool
+    {
+        return School::find($id)->update($data);
+    }
+
+    public function delete(string $id): bool
+    {
+        return School::find($id)->delete();
+    }
+}

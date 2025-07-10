38783df5f6a8cb08115dc744074b7137a17c7582
AndresGutierrezHurtado <andres52885241@gmail.com>
2025-07-10 18:58:43 -0500

    feat: add schools logo cloud section with tooltips and responsive design to enhance visual engagement

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,4 +25,83 @@
         </div>
     </div>
 </section>
+
+<!-- SCHOOLS (LOGO CLOUD) -->
+<section class="w-full px-5">
+    <div class="w-full max-w-[1300px] mx-auto py-20">
+        <div class="flex flex-col md:flex-row items-center gap-10">
+            <div class="space-y-4 w-3/5">
+                <h2 class="text-base-content font-semibold md:text-3xl lg:text-4xl">
+                    Padres que prefieren nuestra calidad para vestir a sus hijos
+                </h2>
+                <p class="text-base-content/80 text-xl">
+                    Cada vez más familias nos eligen por la calidad, comodidad y durabilidad de nuestros uniformes, confeccionados para acompañar el día a día escolar de tus hijos.
+                </p>
+                <a href="#" class="btn btn-primary orm1x icsol">Conoce más</a>
+            </div>
+            <div class="grid grid-cols-3 gap-4 w-2/5">
+                <a href="/schools/id" class="tooltip">
+                    <div class="bg-base-300/10 rounded-box flex items-center justify-center p-5 h-30 cursor-pointer group duration-300 hover:-translate-y-1">
+                        <img src="/images/schools/ensueno.png" alt="Logo del colegio El Ensueño" class="w-full h-full object-contain tooltip-toggle group-hover:scale-110 duration-300 max-h-25">
+                    </div>
+                    <div class="tooltip-content tooltip-shown:opacity-100 tooltip-shown:visible" role="tooltip">
+                        <div class="tooltip-body">
+                            Colegio El Ensueño IED
+                        </div>
+                    </div>
+                </a>
+                <a href="/schools/id" class="tooltip">
+                    <div class="bg-base-300/10 rounded-box flex items-center justify-center p-5 h-30 cursor-pointer group duration-300 hover:-translate-y-1 col-span-2">
+                        <img src="/images/schools/carranza.png" alt="Logo del colegio Carranza" class="w-full h-full object-contain tooltip-toggle group-hover:scale-110 duration-300 max-h-25">
+                    </div>
+                    <div class="tooltip-content tooltip-shown:opacity-100 tooltip-shown:visible" role="tooltip">
+                        <div class="tooltip-body">
+                            Colegio Maria Mercedes Carranza IED
+                        </div>
+                    </div>
+                </a>
+                <a href="/schools/id" class="tooltip">
+                    <div class="bg-base-300/10 rounded-box flex items-center justify-center p-5 h-30 cursor-pointer group duration-300 hover:-translate-y-1 col-span-2">
+                        <img src="/images/schools/caried.svg" alt="Logo del colegio Agudelo Restrepo" class="w-full h-full object-contain tooltip-toggle group-hover:scale-110 duration-300 max-h-25">
+                    </div>
+                    <div class="tooltip-content tooltip-shown:opacity-100 tooltip-shown:visible" role="tooltip">
+                        <div class="tooltip-body">
+                            Colegio Agudelo Restrepo IED
+                        </div>
+                    </div>
+                </a>
+                <a href="/schools/id" class="tooltip">
+                    <div class="bg-base-300/10 rounded-box flex items-center justify-center p-5 h-30 cursor-pointer group duration-300 hover:-translate-y-1">
+                        <img src="/images/schools/currea.png" alt="Logo del colegio Maria Currea" class="w-full h-full object-contain tooltip-toggle group-hover:scale-110 duration-300 max-h-25">
+                    </div>
+                    <div class="tooltip-content tooltip-shown:opacity-100 tooltip-shown:visible" role="tooltip">
+                        <div class="tooltip-body">
+                            Colegio Maria Currea Manrique IED
+                        </div>
+                    </div>
+                </a>
+                <a href="/schools/id" class="tooltip">
+                    <div class="bg-base-300/10 rounded-box flex items-center justify-center p-5 h-30 cursor-pointer group duration-300 hover:-translate-y-1">
+                        <img src="/images/schools/salmona.png" alt="Logo del colegio Rogelio Salmona" class="w-full h-full object-contain tooltip-toggle group-hover:scale-110 duration-300 max-h-25">
+                    </div>
+                    <div class="tooltip-content tooltip-shown:opacity-100 tooltip-shown:visible" role="tooltip">
+                        <div class="tooltip-body">
+                            Colegio Rogelio Salmona IED
+                        </div>
+                    </div>
+                </a>
+                <a href="/schools/id" class="tooltip">
+                    <div class="bg-base-300/10 rounded-box flex items-center justify-center p-5 h-30 cursor-pointer group duration-300 hover:-translate-y-1 col-span-2">
+                        <img src="/images/schools/angela.png" alt="Logo del colegio Angela Restrepo Moreno" class="w-full h-full object-contain tooltip-toggle group-hover:scale-110 duration-300 max-h-25">
+                    </div>
+                    <div class="tooltip-content tooltip-shown:opacity-100 tooltip-shown:visible" role="tooltip">
+                        <div class="tooltip-body">
+                            Colegio Angela Restrepo Moreno IED
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
 @endsection

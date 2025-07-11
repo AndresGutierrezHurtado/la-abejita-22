db71d551260ddd52e4c54e404779f049b7854c6d
AndresGutierrezHurtado <andres52885241@gmail.com>
2025-07-10 19:44:29 -0500

    feat: update welcome page layout and content for improved user engagement and visual appeal

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,18 +7,20 @@
     id="hero"
     class="w-full text-primary-content px-5"
     style="background: linear-gradient(to right, var(--color-primary) 40%, rgba(0,0,0,0.5)), url('/images/foto-seccion.jpg'); background-size: cover; background-position: center;">
-    <div class="w-full max-w-[1300px] mx-auto py-10 md:py-20 lg:py-35">
+    <div class="w-full max-w-[1300px] mx-auto py-10 md:py-20 lg:py-50">
         <div class="max-w-2xl">
-            <h1 class="text-4xl font-bold mb-2">Bienvenido a La Abejita 22</h1>
-            <p class="text-primary-content/80 text-lg mb-6">
+            <h1 class="text-7xl font-bold mb-4">Bienvenido a <br>La Abejita 22</h1>
+            <p class="text-primary-content/80 text-xl mb-8">
                 Descubre nuestra colección exclusiva de productos artesanales y únicos.
                 Cada pieza está creada con amor y dedicación para brindarte calidad y estilo.
             </p>
             <div class="flex flex-col sm:flex-row gap-4">
-                <a href="#productos" class="btn rounded-lg">
-                    Ver Productos
+                <a href="#productos" class="btn btn-lg rounded-lg">
+                    <span class="icon-[tabler--shopping-bag] size-5"></span>
+                    Ver Colegios
                 </a>
-                <a href="#contacto" class="btn btn-outline rounded-lg border-primary-content text-primary-content hover:bg-primary-content hover:text-primary">
+                <a href="#contacto" class="btn btn-lg btn-outline rounded-lg border-primary-content text-primary-content hover:bg-primary-content hover:text-primary">
+                    <span class="icon-[tabler--phone] size-5 shrink-0"></span>
                     Contáctanos
                 </a>
             </div>
@@ -31,13 +33,13 @@
     <div class="w-full max-w-[1300px] mx-auto py-20">
         <div class="flex flex-col md:flex-row items-center gap-10">
             <div class="space-y-4 w-3/5">
-                <h2 class="text-base-content font-semibold md:text-3xl lg:text-4xl">
-                    Padres que prefieren nuestra calidad para vestir a sus hijos
+                <h2 class="text-base-content font-bold md:text-3xl lg:text-4xl">
+                    Padres que prefieren <span class="relative">nuestra calidad <span class="absolute bottom-0 left-0 w-full h-1 bg-gradient-to-r from-primary to-primary/0 rounded-full"></span></span> para vestir a sus hijos
                 </h2>
                 <p class="text-base-content/80 text-xl">
                     Cada vez más familias nos eligen por la calidad, comodidad y durabilidad de nuestros uniformes, confeccionados para acompañar el día a día escolar de tus hijos.
                 </p>
-                <a href="#" class="btn btn-primary orm1x icsol">Conoce más</a>
+                <a href="/schools" class="btn btn-lg btn-primary rounded-lg">Ver más colegios</a>
             </div>
             <div class="grid grid-cols-3 gap-4 w-2/5">
                 <a href="/schools/id" class="tooltip">
@@ -50,8 +52,8 @@
                         </div>
                     </div>
                 </a>
-                <a href="/schools/id" class="tooltip">
-                    <div class="bg-base-300/10 rounded-box flex items-center justify-center p-5 h-30 cursor-pointer group duration-300 hover:-translate-y-1 col-span-2">
+                <a href="/schools/id" class="tooltip col-span-2">
+                    <div class="bg-base-300/10 rounded-box flex items-center justify-center p-5 h-30 cursor-pointer group duration-300 hover:-translate-y-1">
                         <img src="/images/schools/carranza.png" alt="Logo del colegio Carranza" class="w-full h-full object-contain tooltip-toggle group-hover:scale-110 duration-300 max-h-25">
                     </div>
                     <div class="tooltip-content tooltip-shown:opacity-100 tooltip-shown:visible" role="tooltip">
@@ -60,8 +62,8 @@
                         </div>
                     </div>
                 </a>
-                <a href="/schools/id" class="tooltip">
-                    <div class="bg-base-300/10 rounded-box flex items-center justify-center p-5 h-30 cursor-pointer group duration-300 hover:-translate-y-1 col-span-2">
+                <a href="/schools/id" class="tooltip col-span-2">
+                    <div class="bg-base-300/10 rounded-box flex items-center justify-center p-5 h-30 cursor-pointer group duration-300 hover:-translate-y-1">
                         <img src="/images/schools/caried.svg" alt="Logo del colegio Agudelo Restrepo" class="w-full h-full object-contain tooltip-toggle group-hover:scale-110 duration-300 max-h-25">
                     </div>
                     <div class="tooltip-content tooltip-shown:opacity-100 tooltip-shown:visible" role="tooltip">
@@ -90,8 +92,8 @@
                         </div>
                     </div>
                 </a>
-                <a href="/schools/id" class="tooltip">
-                    <div class="bg-base-300/10 rounded-box flex items-center justify-center p-5 h-30 cursor-pointer group duration-300 hover:-translate-y-1 col-span-2">
+                <a href="/schools/id" class="tooltip col-span-2">
+                    <div class="bg-base-300/10 rounded-box flex items-center justify-center p-5 h-30 cursor-pointer group duration-300 hover:-translate-y-1">
                         <img src="/images/schools/angela.png" alt="Logo del colegio Angela Restrepo Moreno" class="w-full h-full object-contain tooltip-toggle group-hover:scale-110 duration-300 max-h-25">
                     </div>
                     <div class="tooltip-content tooltip-shown:opacity-100 tooltip-shown:visible" role="tooltip">
